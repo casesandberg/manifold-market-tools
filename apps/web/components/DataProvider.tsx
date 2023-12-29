b09be7aa6e1f467cf020be463488e979ac1d4a53
case <case@casesandberg.com>
2023-12-29 14:09:54 -0800

    Add Websocket

--- apps/web/components/DataProvider.tsx
+++ apps/web/components/DataProvider.tsx
@@ -1,14 +1,39 @@
 'use client'
 
-import { db } from '@/lib/db'
+import { Market, db } from '@/lib/db'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { useLocalStorage } from 'react-use'
+import { Socket } from 'phoenix'
 
 const DataContext = createContext<{ isSyncing: boolean } | null>(null)
 
 export function DataProvider({ children }: { children: React.ReactNode }) {
   const [isSyncing, setIsSyncing] = useState(false)
   const [value, setValue] = useLocalStorage<Date>('MARKETS_LAST_FETCH')
+
+  useEffect(() => {
+    let socket = new Socket('ws://nyr.fly.dev/socket', { params: {} })
+    socket.connect()
+
+    let channel = socket.channel('updates:markets', {})
+    channel
+      .join()
+      .receive('ok', (resp) => {
+        console.log('Joined successfully', resp)
+      })
+      .receive('error', (resp) => {
+        console.log('Unable to join', resp)
+      })
+
+    channel.on('market_updated', (market: Market) => {
+      db.markets.put(market)
+    })
+
+    return () => {
+      channel.leave()
+      socket.disconnect()
+    }
+  }, [])
 
   useEffect(() => {
     setIsSyncing(true)
