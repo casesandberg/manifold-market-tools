6abba50f75b4e0233d26e135f84f15f8028beb07
case <case@casesandberg.com>
2023-12-30 21:37:18 -0800

    Basic Mobile Styles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,28 +16,30 @@ export default async function Home() {
   return (
     <main>
       <div className="mt-8 flex gap-12 border-b pb-8">
-        <div className="flex-[3] lg:order-2">
-          <MarketsStats initialTotal={stats.total_markets} initialResolved={stats.resolved_markets} />
-        </div>
-        <div className="flex-1 lg:order-1">
+        <div className="flex-1">
           <div className="overflow-hidden">
             <dt className="truncate text-sm font-medium text-gray-500">Manifold{"'"}s New Years</dt>
             <dd className="mt-1 font-serif text-3xl font-semibold tracking-tight text-gray-900">Resolutions</dd>
           </div>
         </div>
-        <div className="flex-1 lg:order-3"></div>
+        <div className="flex-[3]">
+          <MarketsStats initialTotal={stats.total_markets} initialResolved={stats.resolved_markets} />
+        </div>
+
+        <div className="flex-1"></div>
       </div>
       <div className="mt-8 flex gap-12">
-        <div className="flex min-w-0 flex-[3] flex-col gap-8 lg:order-2">
+        <div className="hidden flex-1 md:block">
+          <RecentlyResolvedList />
+        </div>
+        <div className="flex min-w-0 flex-[3] flex-col gap-8">
           <div className="h-[250px]">
             <ResolvedMarketsGraph />
           </div>
           <MarketsDotGrid />
         </div>
-        <div className="flex-1 lg:order-1">
-          <RecentlyResolvedList />
-        </div>
-        <div className="flex-1 lg:order-3">
+
+        <div className="hidden flex-1 lg:block">
           <Leaderboard />
         </div>
       </div>
